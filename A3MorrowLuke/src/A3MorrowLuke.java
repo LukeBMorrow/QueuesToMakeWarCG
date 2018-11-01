ea05f77f6ccbc24655164e9cc4e3f1ff5b475d6e
lukebmorrow <lukebmorrow@gmail.com>
2018-11-01 12:25:13 -0500

    added a card class with a toString and a way to compare card values.

--- A3MorrowLuke/src/A3MorrowLuke.java
+++ A3MorrowLuke/src/A3MorrowLuke.java
@@ -6,16 +6,14 @@ public class A3MorrowLuke {
     }
 }
 
-class Deck{
-
-    public final int SPADES = 0, DIAMONDS = 1, HEARTS = 2, CLUBS =3;
-    public final int ACE = 1, JACK = 11, QUEEN = 12, KING = 13;
+/*
+ */
+class Deck
+{
     private final int NUM_SHUFFLES = 52;
     private final int STANDARD_DECK_SIZE = 52;
-    private final int SUIT_SIZE = 13;
-    private final int NUM_SUITS = 4;
 
-    int[] localDeck;
+    Card[] localDeck;
 
     public Deck()
     {
@@ -25,9 +23,9 @@ class Deck{
 
     private void make(int size)
     {
-       localDeck = new int[size];
-        for(int i: localDeck)
-        {localDeck[i]=i+1;}
+       localDeck = new Card[size];
+        for(int i = 0; i<localDeck.length; i++)
+        {localDeck[i]=new Card(i);}
     }
 
     private void shuffle()
@@ -38,23 +36,17 @@ class Deck{
 
     private void swap(int a, int b)
     {
-        int temp = localDeck[a];
+        Card temp = localDeck[a];
         localDeck[a]=localDeck[b];
         localDeck[b]=temp;
     }
-
-    public int getSuit(int a)
-    { return a/SUIT_SIZE;}
-
-    public int getValue(int a)
-    { return a%NUM_SUITS +1;} //+1 b/c mod start at 0, and 0 isn't a card
 
     public void dealHands(Hand a, Hand b)
     {
         for(int i = 0; i<localDeck.length; i++)
         {
 
-            if(i/Hand.HAND_SIZE == 0)
+            if(i/(localDeck.length/2) == 0)
                 a.enter(localDeck[i]);
             else
                 b.enter(localDeck[i]);
@@ -62,33 +54,77 @@ class Deck{
     }
 }
 
+
+/*
+ */
+class Card
+{
+    private String[] SUIT_TRANSLATION = {"S", "D", "H", "C"};
+    private String[] VALUE_TRANSLATION = {"A","2","3","4","5","6","7","8","9","10","J","Q","K"};
+    private final int SUIT_SIZE = 13;
+    private final int NUM_SUITS = 4;
+
+    private String suit;
+    private int value;
+
+
+    public Card(int cardNum)
+    {
+        suit = SUIT_TRANSLATION[cardNum/NUM_SUITS];
+        value = cardNum%SUIT_SIZE +1;//+1 b/c mod start at 0, and 0 isn't a card
+    }
+
+
+    public int getValue() {
+        return value;
+    }
+
+    /*A chunk used to compare the values of cards */
+    public boolean isGreaterThan(Card a)
+    {return value > a.getValue();}
+    public boolean isLessThan(Card a)
+    {return value < a.getValue();}
+    public boolean isEqualTo(Card a)
+    {return value == a.getValue();}
+
+    @Override
+    public String toString() {
+        return "{"+VALUE_TRANSLATION[value] +":"+ suit+"}";
+    }
+}
+
+
+
+/*
+ */
 class Hand
 {
     public static final int HAND_SIZE = 33;
     private int start;
     private int end;
-    int[] queue;
+    Card[] queue;
 
 
     public Hand()
     {
-            queue = new int[HAND_SIZE];
+            queue = new Card[HAND_SIZE];
             start=0;
-            end=HAND_SIZE-1;//-1 b/c we want the last position of the array
+            end=start;
     }
 
-    public void enter(int a)
+    public void enter(Card a)
     {
-        assert(a<52 && a>=0);
-        end = a;
+        queue[end] = a;
         end = (end++)%queue.length;
     }
 
-    public int leave()
+    public Card leave()
     {
-        int storage = queue[start];
-        queue[start]=-1;//remove the item
+        Card storage = queue[start];
+
+        queue[start]=null;//remove the item
         start = (start++)%queue.length;//point at the next in line
+
         return storage;
     }
 
